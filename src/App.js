b4f05954234c9bf2efad0a8807d0b8e527430306
VPhu <phulv08594@gmail.com>
2025-04-24 11:15:41 +0700

    Feat: Main Page Frame

--- src/App.js
+++ src/App.js
@@ -4,7 +4,10 @@ import GlobalStyles from "./pages/GlobalStyles/index";
 import HomePage from "./pages/HomePage/HomePage";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
+import Test from "../src/pages/HomePage/test";
 // import Profile from "./pages/Profile";
+import '@fortawesome/fontawesome-free/css/all.min.css';
+
 
 function App() {
   return (
@@ -14,6 +17,8 @@ function App() {
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/Signup" element={<Signup />} />
+          <Route path="/test" element={<Test />} />
+
         </Routes>
       </Router>
     </GlobalStyles>
