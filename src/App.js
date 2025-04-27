ca45daf4c28f9a7b90ae249a9878111afcd0e5e5
VPhu <phulv08594@gmail.com>
2025-04-27 09:41:34 +0700

    Feat: Profile Page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import GlobalStyles from "./pages/GlobalStyles/index";
 import HomePage from "./pages/HomePage/HomePage";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
+import Profile from "./pages/ProfilePage/ProfilePage";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
 
@@ -14,8 +15,8 @@ function App() {
         <Routes>
           <Route path="/" element={<HomePage />} />
           <Route path="/login" element={<Login />} />
-          <Route path="/signup" element={<Signup />} />
-
+          <Route path="/Signup" element={<Signup />} />
+          <Route path="/Profile" element={<Profile />} />
         </Routes>
       </Router>
     </GlobalStyles>
