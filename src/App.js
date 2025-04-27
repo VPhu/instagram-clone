fea58f56c9ca61b35407dedd4db23c5241d31b65
VPhu <phulv08594@gmail.com>
2025-04-27 10:47:11 +0700

    feature/update-login&signup

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import GlobalStyles from "./pages/GlobalStyles/index";
 import HomePage from "./pages/HomePage/HomePage";
 import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
-// import Profile from "./pages/Profile";
 
 function App() {
   return (
