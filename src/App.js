971f70e8756b81b07b84be849ce187387eee3e7d
VPhu <phulv08594@gmail.com>
2025-05-27 10:26:19 +0700

    Feat: Create login page logic

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Login from "./pages/Login/Login";
 import Signup from "./pages/Signup/Signup";
 import Profile from "./pages/ProfilePage/ProfilePage";
 import Explore from "./pages/Explore/Explore";
+import PrivateRoute from "./component/layout/PrivateRoute";
+import PublicRoute from "./component/layout/PublicRoute";
 import { ToastContainer } from "react-toastify";
 import '@fortawesome/fontawesome-free/css/all.min.css';
 
@@ -15,11 +17,11 @@ function App() {
     <GlobalStyles>
       <Router>
         <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/login" element={<Login />} />
-          <Route path="/Signup" element={<Signup />} />
-          <Route path="/Profile" element={<Profile />} />
-          <Route path="/explore" element={<Explore/>}/>
+          <Route path="/" element={<PrivateRoute><HomePage /></PrivateRoute>} />
+          <Route path="/login" element={<PublicRoute><Login /></PublicRoute>} />
+          <Route path="/Signup" element={<PublicRoute><Signup /></PublicRoute>} />
+          <Route path="/Profile" element={<PrivateRoute><Profile /></PrivateRoute>} />
+          <Route path="/explore" element={<PrivateRoute><Explore/></PrivateRoute>}/>
         </Routes>
       </Router>
       <ToastContainer/>
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,16 +1,72 @@
-import React from "react";
+import React, { useCallback, useEffect } from "react";
 import { useState } from "react";
 import { useNavigate, Link } from "react-router-dom";
+import { toast } from "react-toastify";
+import { setToken } from "../../utils/cookie";
 import Footer from "../../component/layout/Footer/Footer";
 import styles from "./Login.module.css";
 
 const Login = () => {
-  const [email, setEmail] = useState("");
+  const [emailOrPhone, setEmailOrPhone] = useState("");
   const [password, setPassword] = useState("");
+  const [showPass, setShowPass] = useState(false);
+  const [errorLogin, setErrorsLogin] = useState(false);
+  const [validInfo, setValidInfo] = useState(false);
   const navigate = useNavigate();
 
-  const handleLogin = () => {
-    
+  const handleChangeInput = (e, setState) => {
+    const value = e.target.value;
+    setState(value);
+    setErrorsLogin(false)
+  };
+  const handleShowPass = () => {
+    setShowPass(!showPass);
+  };
+
+  const checkForm = useCallback(() => {
+    const isEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(emailOrPhone);
+    const isPhone = /^(\+?\d{8,15})$/.test(emailOrPhone);
+    return (isEmail || isPhone) && password.length > 5;
+  }, [emailOrPhone, password]);
+
+  useEffect(() => {
+    checkForm() ? setValidInfo(true) : setValidInfo(false);
+  }, [checkForm]);
+
+  const handleLogin = async () => {
+    if (!validInfo) {
+      return;
+    }
+    try {
+      const response = await fetch("http://localhost:3000/api/auth/login", {
+        method: "POST",
+        headers: {
+          "Cache-Control": "no-cache",
+          "Content-Type": "application/json",
+          "Accept": "*/*",
+        },
+        body: JSON.stringify({
+          email: emailOrPhone,
+          password,
+        }),
+      });
+
+      const data = await response.json();
+      if (response.ok) {
+        toast.success("Login successful!");
+        setTimeout(()=>{
+          setToken(data.token)
+          localStorage.setItem("user",JSON.stringify(data.user))
+          navigate("/")   
+        }, 3000)
+      } else {
+        if (data.message) {
+          setErrorsLogin(true);
+        }
+      }
+    } catch (error) {
+      toast.error(error);
+    }
   };
 
   return (
@@ -20,7 +76,7 @@ const Login = () => {
           <div className={styles.imageSectionWrapper}>
             <div className={styles.imageSection}>
               <img
-                src="/images/instagram_mockup.png"
+                src="https://media.gcflearnfree.org/content/633d944b3823fb02e84dce55_10_05_2022/Screen%20Shot%202022-10-10%20at%202.28.19%20PM.png"
                 alt="Instagram Mockup"
                 className={styles.mockupImg}
               />
@@ -33,28 +89,51 @@ const Login = () => {
                 <div className="logoInstagramLogin"></div>
               </div>
               <div className={styles.formLogin}>
-                <input
-                  type="email"
-                  className="inputField"
-                  placeholder="Email"
-                  onChange={(e) => {
-                    setEmail(e.target.value);
-                  }}
-                />
-                <input
-                  type="password"
-                  className="inputField"
-                  placeholder="Password"
-                  onChange={(e) => {
-                    setPassword(e.target.value);
-                  }}
-                />
+                <div className={styles.inputLoginWrapper}>
+                  <input
+                    type="email"
+                    className="inputField"
+                    placeholder="Phone number, or email"
+                    onChange={(e) => {
+                      handleChangeInput(e, setEmailOrPhone);
+                    }}
+                  />
+                </div>
+                <div className={styles.inputLoginWrapper}>
+                  <input
+                    type={showPass ? "text" : "password"}
+                    className="inputField"
+                    placeholder="Password"
+                    onCopy={showPass ? (e) => e.preventDefault() : undefined}
+                    onChange={(e) => {
+                      handleChangeInput(e, setPassword);
+                    }}
+                  />
+                  {password.length > 0 && (
+                    <div className={styles.passStatus} onClick={handleShowPass}>
+                      {showPass ? (
+                        <span className={styles.passStatusText}>Hide</span>
+                      ) : (
+                        <span className={styles.passStatusText}>Display</span>
+                      )}
+                    </div>
+                  )}
+                </div>
               </div>
-              <button className="btnSubmit" onClick={handleLogin}>
+              <button
+                className={`btnSubmit ${validInfo ? "btnSubmitValid" : ""}`}
+                onClick={handleLogin}
+              >
                 Log in
               </button>
               <p className="orText">OR</p>
               <button className="fbLoginBtn">Log in with Facebook</button>
+              {errorLogin && (
+                <span className={styles.errorMessage}>
+                  Sorry, your password was incorrect. Please double-check your
+                  password.
+                </span>
+              )}
               <p className={styles.forgotPassword}>
                 <Link to="/forgotpassword">Forgot password?</Link>
               </p>
